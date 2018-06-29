c5108dfaeafb40a98f633bc103335a76d142e35b
Linya <avt1745zeo@gmail.com>
2018-06-29 15:14:59 +0800

    Test index.html.

---
+++ res/js/loading.js
@@ -0,0 +1,18 @@
+﻿$(window).on('load', function () {
+
+    /*Fade loading*/
+
+    var fadeLoading = function () {
+        $('#loading').fadeOut(3000);
+    }
+
+    /*Show content space*/
+
+    var showContentSpace = function () {
+        $('#contentSpace').fadeIn(2000);
+        $('#content-home').fadeIn(3000);
+    }
+
+    setTimeout(fadeLoading,1000);
+    setTimeout(showContentSpace,3000);
+})
