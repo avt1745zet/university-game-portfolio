4eec4b7920ea442590af881c076f0126b626bfce
Linya <avt1745zeo@gmail.com>
2019-05-30 21:51:00 +0800

    1. Change all image of level content. 2. Add js-scroll-show a data "scroll-show-range"(default value 100). 3. Remove unused image.

--- res/js/portfolio.js
+++ res/js/portfolio.js
@@ -46,7 +46,11 @@
     function showJsScrollShowIfInWindow() {
         $(".js-scroll-show").each(function () {
             var target = this;
-            elementOffsetInWindow(target, 80, function () { show(target); });
+            var range = 100;
+            var data = $(target).data("scroll-show-range");
+            if (data)
+                range = data;
+            elementOffsetInWindow(target, range, function () { show(target); });
         });
 
         function show(target) {
@@ -62,20 +66,7 @@
     });
 });
 
-function elementHeightInWindow(target, percentage = 0, callback) {
-    var windowHeight = $(window).height();
-    var windowBottomScrollTop = $(window).scrollTop() + windowHeight;
-    $(target).each(function () {
-        var targetHeight = $(target).height();
-        var targetCenter = $(target).offset().top + targetHeight / 2;
-        var curPencentage = -(targetCenter - windowBottomScrollTop) / windowHeight * 100;
-        if (curPencentage > percentage) {
-            callback();
-        }
-    });
-}
-
-function elementOffsetInWindow(target, percentage = 0, callback) {
+function elementOffsetInWindow(target, percentage = 10, callback) {
     var windowHeight = $(window).height();
     var windowCenterScrollTop = $(window).scrollTop() + windowHeight / 2;
     $(target).each(function () {
