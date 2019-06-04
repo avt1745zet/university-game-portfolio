4c9f4360adcd03916cfb65dc6e599900bb258c39
Linya <avt1745zeo@gmail.com>
2019-06-04 18:11:32 +0800

    Update big rich man page.

--- bigrichman/res/js/bigrichman.js
+++ bigrichman/res/js/bigrichman.js
@@ -1,3 +1,18 @@
 ﻿$(document).ready(function () {
-    $("div").animate({ bottom: "10px", opacity: "1" }, 'slow');
+    $(".character-img").mouseenter(function () {
+        var target = $(this);
+        var characterName = target.data("character-name");
+        var src = target.prop("src");
+        var newSrc = src.slice(0, src.lastIndexOf("/") + 1);
+        newSrc += characterName.toString().concat("_walk.gif");
+        target.prop("src", newSrc);
+    });
+    $(".character-img").mouseleave(function () {
+        var target = $(this);
+        var characterName = target.data("character-name");
+        var src = target.prop("src");
+        var newSrc = src.slice(0, src.lastIndexOf("/") + 1);
+        newSrc += characterName.toString().concat("_idle.png");
+        target.prop("src", newSrc);
+    });
 });
