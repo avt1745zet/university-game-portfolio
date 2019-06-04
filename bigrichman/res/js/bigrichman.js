25157a0a22eb0030f955973a6fa14e9bde91e043
Linya <avt1745zeo@gmail.com>
2019-06-04 22:12:27 +0800

    Update big rich man page.

--- bigrichman/res/js/bigrichman.js
+++ bigrichman/res/js/bigrichman.js
@@ -1,4 +1,7 @@
 ﻿$(document).ready(function () {
+    /*
+     * 
+     */
     $(".character-img").mouseenter(function () {
         var target = $(this);
         var characterName = target.data("character-name");
@@ -14,5 +17,51 @@
         var newSrc = src.slice(0, src.lastIndexOf("/") + 1);
         newSrc += characterName.toString().concat("_idle.png");
         target.prop("src", newSrc);
+    });
+    /*
+     * 
+     */
+    $('.fate-card').magnificPopup({
+        type: 'image',
+        tLoading: 'Loading image #%curr%...',
+        mainClass: 'mfp-img-mobile mfp-with-zoom',
+        gallery: {
+            enabled: true,
+            navigateByImgClick: true,
+            preload: [0, 1] // Will preload 0 - before current, and 1 after the current image
+        },
+        image: {
+            tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
+            titleSrc: function (item) {
+                return item.el.attr('title');
+            }
+        },
+        zoom: {
+            enabled: true,
+            duration: 300 // don't foget to change the duration also in CSS
+        }
+    });
+    /*
+     * 
+     */
+    $('.chance-card').magnificPopup({
+        type: 'image',
+        tLoading: 'Loading image #%curr%...',
+        mainClass: 'mfp-img-mobile mfp-with-zoom',
+        gallery: {
+            enabled: true,
+            navigateByImgClick: true,
+            preload: [0, 1] // Will preload 0 - before current, and 1 after the current image
+        },
+        image: {
+            tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
+            titleSrc: function (item) {
+                return item.el.attr('title');
+            }
+        },
+        zoom: {
+            enabled: true,
+            duration: 300 // don't foget to change the duration also in CSS
+        }
     });
 });
