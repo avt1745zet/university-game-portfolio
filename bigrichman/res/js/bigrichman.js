d62811802c6961290fb9c32ef2832c1b48ee660e
Linya <avt1745zeo@gmail.com>
2019-06-08 00:58:17 +0800

    1. Update big rich man page and tetris in 3D page. 2. Replace Linya Studio to 羊羽的小窩.

--- bigrichman/res/js/bigrichman.js
+++ bigrichman/res/js/bigrichman.js
@@ -64,4 +64,27 @@
             duration: 300 // don't foget to change the duration also in CSS
         }
     });
+    /*
+     * Game display magnificPopup.
+     */
+    $('.game-display').magnificPopup({
+        type: 'image',
+        tLoading: 'Loading image #%curr%...',
+        mainClass: 'mfp-img-mobile mfp-with-zoom',
+        gallery: {
+            enabled: true,
+            navigateByImgClick: true,
+            preload: [0, 1] // Will preload 0 - before current, and 1 after the current image
+        },
+        image: {
+            tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
+            titleSrc: function (item) {
+                return item.el.attr('title');
+            }
+        },
+        zoom: {
+            enabled: true,
+            duration: 300 // don't foget to change the duration also in CSS
+        }
+    });
 });
--- tetrisin3d/res/js/tetrisin3d.js
+++ tetrisin3d/res/js/tetrisin3d.js
@@ -1,1 +1,31 @@
-﻿
+﻿$(document).ready(function () {
+    var originalText = $(".test").text();
+    var currentText = "";
+    var prefix = "|";
+
+    clear();
+    keying(prefix);
+
+    function clear() {
+        $(".test").text("");
+    }
+
+    function keying(prefix = "") {
+        setTimeout(function () {
+            currentText = currentText + originalText.charAt(currentText.length);
+            $(".test").text(currentText + prefix);
+
+            playKeyAudio();
+
+            if (currentText === originalText === false)
+                keying(prefix);
+            else
+                $(".test").text(currentText);
+        }, 250);
+    }
+
+    function playKeyAudio() {
+        document.getElementById("key-audio").currentTime = 0;
+        document.getElementById("key-audio").play();
+    }
+});
