137dc9d410363619a4fdcb3e579f2e90bf89186a
Linya <avt1745zeo@gmail.com>
2019-06-09 02:04:20 +0800

    Update dream page.

--- dream/res/js/dream.js
+++ dream/res/js/dream.js
@@ -1,1 +1,34 @@
-﻿
+﻿$(document).ready(function () {
+
+    firstSink();
+
+    function firstSink() {
+        setTimeout(function () { sink($("#D")); }, 400);
+        setTimeout(function () { sink($("#r")); }, 1000);
+        setTimeout(function () { sink($("#e")); }, 600);
+        setTimeout(function () { sink($("#a")); }, 200);
+        setTimeout(function () { sink($("#m")); }, 800);
+    }
+
+    setTimeout(function () { keepDream(); }, 1000);
+
+    function keepDream() {
+        keep($("#D"));
+        keep($("#r"));
+        keep($("#e"));
+        keep($("#a"));
+        keep($("#m"));
+        setTimeout(function () { keepDream(); }, 6000);
+        console.log("1");
+    }
+
+
+    function sink(target) {
+        target.animate({ top: "45%" }, 3000, "easeOutBack");
+    }
+
+    function keep(target) {
+        target.animate({ top: "50%" }, 3000, "easeOutCubic");
+        target.animate({ top: "45%" }, 3000, "easeInCubic");
+    }
+});
--- tetrisin3d/res/js/tetrisin3d.js
+++ tetrisin3d/res/js/tetrisin3d.js
@@ -15,17 +15,10 @@
             currentText = currentText + originalText.charAt(currentText.length);
             $(".test").text(currentText + prefix);
 
-            playKeyAudio();
-
             if (currentText === originalText === false)
                 keying(prefix);
             else
                 $(".test").text(currentText);
         }, 250);
-    }
-
-    function playKeyAudio() {
-        document.getElementById("key-audio").currentTime = 0;
-        document.getElementById("key-audio").play();
     }
 });
