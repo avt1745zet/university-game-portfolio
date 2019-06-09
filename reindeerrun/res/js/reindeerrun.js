8b7a50195e38d60680d9511daf1b82cf1eec8aa9
Linya <avt1745zeo@gmail.com>
2019-06-09 23:11:07 +0800

    Update reindeer run page.

--- reindeerrun/res/js/reindeerrun.js
+++ reindeerrun/res/js/reindeerrun.js
@@ -1,1 +1,19 @@
-﻿
+﻿$(document).ready(function () {
+    /*
+     * Hide title.
+     */
+    $("#title").hide();
+    $("#title").fadeIn(3000);
+    /*
+     * popup-youtube
+     */
+    $('.popup-youtube, .popup-vimeo, .popup-gmaps').magnificPopup({
+        disableOn: 700,
+        type: 'iframe',
+        mainClass: 'mfp-fade',
+        removalDelay: 160,
+        preloader: false,
+
+        fixedContentPos: true
+    });
+}); 
