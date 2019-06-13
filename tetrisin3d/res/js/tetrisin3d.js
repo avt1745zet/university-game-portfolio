33ebf1e7a302cb73b2a216ad47b301446f57d6d2
Linya <avt1745zeo@gmail.com>
2019-06-13 21:45:16 +0800

    Tetrisin3d page is done.

--- tetrisin3d/res/js/tetrisin3d.js
+++ tetrisin3d/res/js/tetrisin3d.js
@@ -1,24 +1,49 @@
-﻿$(document).ready(function () {
-    var originalText = $(".test").text();
-    var currentText = "";
-    var prefix = "|";
+﻿/*
+ * Keying effect.
+ */
+$(document).ready(function () {
+    $(".js-keying-effect").each(function () {
+        var cursor = $(this).data("cursor");
+        var timeInterval = $(this).data("interval");
+        var originalText = $(this).text();
 
-    clear();
-    keying(prefix);
+        clearText(this);
+        keyingEffect(this, originalText, cursor, timeInterval);
 
-    function clear() {
-        $(".test").text("");
-    }
+        function clearText(target) {
+            $(target).text("");
+        }
+        
+        function keyingEffect(target, goalText, cursor = "", timeInterval = 100) {
+            /*
+             * Do things at first.
+             */
+            var textSpan = $(target).children(".js-inner-text");
+            if (textSpan.length === 0) {
+                $(target).append("<span class='js-inner-text'>" + target.innerText + "</span>");
+                textSpan = $(target).children(".js-inner-text");
+            }
 
-    function keying(prefix = "") {
-        setTimeout(function () {
-            currentText = currentText + originalText.charAt(currentText.length);
-            $(".test").text(currentText + prefix);
+            var cursorSpan = $(target).children(".js-cursor");
+            if (cursorSpan.length === 0) {
+                console.log(cursorSpan.length);
+                $(target).append("<span class='js-cursor'>" + cursor + "</span>");
+                cursorSpan = $(target).children(".js-cursor");
+            } 
+            /*
+             * Do things after interval.
+             */
+            setTimeout(function () {
+                var resultText = textSpan.text() + goalText.charAt(textSpan.text().length);
+                textSpan.text(resultText);
 
-            if (currentText === originalText === false)
-                keying(prefix);
-            else
-                $(".test").text(currentText);
-        }, 250);
-    }
+                if (resultText === goalText === true) {
+                    cursorSpan.remove();
+                    $(target).text(textSpan.text());
+                }
+                else
+                    keyingEffect(target, goalText, cursor, timeInterval);
+            }, timeInterval);
+        }
+    });
 });
